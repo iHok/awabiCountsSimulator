2fd8358b0dc804dc6f2fa2fdd7b035f4def7bb2c
iHok <ihokya@gmail.com>
2020-01-26 22:44:17 +0900

    とりあえず途中段階

--- docs/assets/script.js
+++ docs/assets/script.js
@@ -4,3 +4,22 @@ let dividedBy50 = (x) => {return x/50}
 
 let setValue = (x,y) => {document.querySelector(x).setAttribute('value', y)}
 
+let generate = () =>{
+    let getInput = document.querySelectorAll("#getInput input");
+    let setInput = document.querySelectorAll("#setInput input");
+
+        console.dir(getInput);
+        getInput.forEach((e,i) =>{
+            console.log(e.name);
+            console.log(e.name=="kaisu");
+            if(e.name=="kaisu"){
+                setValue( '#'+e.name , e.value)
+            } else {
+                setValue( '#'+e.name , dividedBy50(round25(e.value)))
+            };
+        })
+//    setValue( '#eiki' , dividedBy50(round25(this.value)));
+
+}
+
+generate();
