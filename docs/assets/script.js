a33465fa9ecab6d3dca6a7d149d1c6681467a873
iHok <ihokya@gmail.com>
2020-01-27 02:29:28 +0900

    genelateをcalccalcに＋その他

--- docs/assets/script.js
+++ docs/assets/script.js
@@ -4,22 +4,23 @@ let dividedBy50 = (x) => {return x/50}
 
 let setValue = (x,y) => {document.querySelector(x).setAttribute('value', y)}
 
-let generate = () =>{
+let calc = () =>{
     let getInput = document.querySelectorAll("#getInput input");
-    let setInput = document.querySelectorAll("#setInput input");
 
-        console.dir(getInput);
-        getInput.forEach((e,i) =>{
-            console.log(e.name);
-            console.log(e.name=="kaisu");
-            if(e.name=="kaisu"){
-                setValue( '#'+e.name , e.value)
-            } else {
-                setValue( '#'+e.name , dividedBy50(round25(e.value)))
-            };
-        })
-//    setValue( '#eiki' , dividedBy50(round25(this.value)));
-
+    getInput.forEach((e,i) =>{
+        if(e.name=="kaisu"){
+            setValue( '#'+e.name , e.value)
+        } else {
+            setValue( '#'+e.name , dividedBy50(round25(e.value)))
+        };
+    })
 }
 
-generate();
+let generate = () => {
+    let getSelect = document.querySelectorAll(".select select");
+    getSelect.forEach((e,i)=>{
+        console.dir(e.value);
+    })
+}
+
+calc();
