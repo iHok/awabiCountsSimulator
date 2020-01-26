01f66f4853e87538f15c9c55a5445e960f93abcb
iHok <ihokya@gmail.com>
2020-01-26 12:10:57 +0900

    とりあえず入力値の繁栄までできた

---
+++ docs/assets/script.js
@@ -0,0 +1,6 @@
+let round25 = (x) => {return 0 <= x ? x-x%25 : (x-25)-x%25}
+
+let dividedBy50 = (x) => {return x/50}
+
+let setValue = (x,y) => {document.querySelector(x).setAttribute('value', y)}
+
