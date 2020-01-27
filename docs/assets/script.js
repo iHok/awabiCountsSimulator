1b98484ed711be88998dd6d8026c8d6b851a8889
iHok <ihokya@gmail.com>
2020-01-27 20:36:27 +0900

    とりあえず動いている感じ

--- docs/assets/script.js
+++ docs/assets/script.js
@@ -5,22 +5,38 @@ let dividedBy50 = (x) => {return x/50}
 let setValue = (x,y) => {document.querySelector(x).setAttribute('value', y)}
 
 let calc = () =>{
-    let getInput = document.querySelectorAll("#getInput input");
+    let getInputHouju = document.querySelectorAll("#getInput input:not([name='回数']):not([name='合計'])");
+    let kaisu = document.querySelector("#getInput input[name='回数']").value;
+    let houjuGenerate = document.querySelectorAll("#generateResult tr");
+    let goukei = 0;
+    let mainGenerate = 0;
+    let subGenerate = 0;
+    let goukeiZansu = 0;
 
-    getInput.forEach((e,i) =>{
-        if(e.name=="kaisu"){
-            setValue( '#'+e.name , e.value)
-        } else {
-            setValue( '#'+e.name , dividedBy50(round25(e.value)))
-        };
+    getInputHouju.forEach((e) => {
+        goukei = Number(goukei) + Number(e.value);
+        mainGenerate = document.querySelectorAll(".main." + e.name).length;
+        subGenerate = document.querySelectorAll(".sub." + e.name).length / 2;
+        goukeiZansu = Number(goukeiZansu) + Number(dividedBy50(round25(e.value))-mainGenerate-subGenerate)
+        setValue( '#'+e.name , Number(dividedBy50(round25(e.value)) - Number(mainGenerate) - Number(subGenerate)))
     })
+    setValue('#合計', goukei);
+    setValue('#回数', Number(kaisu) + Number(houjuGenerate.length));
+    setValue('#合計残数', goukeiZansu);
 }
 
 let generate = () => {
+    let tableGenerate = document.querySelector("#generateResult"); 
     let getSelect = document.querySelectorAll(".select select");
-    getSelect.forEach((e,i)=>{
-        console.dir(e.value);
-    })
+    const trGenerate = `
+    <tr>
+        <td class="main ${getSelect[0].value}">${getSelect[0].value}</td>
+        <td class="sub ${getSelect[1].value}">${getSelect[1].value}</td>
+        <td class="sub ${getSelect[2].value}">${getSelect[2].value}</td>
+        <td><input type="button" value="削除" onclick="this.parentNode.parentNode.remove();calc();"></td>
+    </tr>
+    `;
+    tableGenerate.insertAdjacentHTML('beforeend', trGenerate); 
 }
 
 calc();
